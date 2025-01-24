75b91cbf4009e43bbe2b0f8eaf5d690ce2751ea4
RazvanDRL <razdir97@gmail.com>
2025-01-24 23:36:03 +0200

    add x pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -99,6 +99,16 @@ export default function RootLayout({
       <head>
         <IntercomScript />
         <meta name="apple-mobile-web-app-title" content="UGC Farm" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+              },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+              a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+              twq('config','p1g9z');
+            `
+          }}
+        />
       </head>
       <PostHogProvider>
         <body className={`${satoshi.className}`}>
